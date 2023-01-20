d7c38e9bf85e6e30b8c5a2e796dac3dcb6fc0690
Rahul Biswakarma <rahul.id39@gmail.com>
2023-01-20 15:57:48 +0530

    added more features and animation in about page

--- scripts/about.js
+++ scripts/about.js
@@ -33,6 +33,36 @@ window.onload = function () {
 document.addEventListener("DOMContentLoaded", function () {
   var lazyImages = [].slice.call(document.querySelectorAll("img.lazy"));
 
+  var sliderImages = document.querySelectorAll(".slider-image");
+  var currentImage = 0;
+  var prevX = 0;
+  var prevY = 0;
+  window.setInterval(function () {
+    // get random location inside header tag
+    var randomX = Math.floor(Math.random() * 80 + 10);
+    var randomY = Math.floor(Math.random() * 80 + 10);
+
+    while (Math.abs(prevX - randomX) > 100)
+      randomX = Math.floor(Math.random() * 80 + 10);
+
+    while (Math.abs(prevY - randomY) > 100)
+      randomY = Math.floor(Math.random() * 80 + 10);
+
+    sliderImages[currentImage].style.top = randomY + "%";
+    sliderImages[currentImage].style.left = randomX + "%";
+    sliderImages[currentImage].classList.add("animate");
+    currentImage = (currentImage + 1) % sliderImages.length;
+
+    prevX = randomX;
+    prevY = randomY;
+  }, 500);
+
+  sliderImages.forEach((image) => {
+    image.addEventListener("animationend", () => {
+      image.classList.remove("animate");
+    });
+  });
+
   if ("IntersectionObserver" in window) {
     let lazyImageObserver = new IntersectionObserver(function (
       entries,
@@ -54,5 +84,32 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   } else {
     // Possibly fall back to event handlers here
+  }
+});
+
+function ValidateEmail(mail) {
+  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail)) {
+    return true;
+  }
+  return false;
+}
+
+document.getElementById("email-input").addEventListener("click", () => {
+  document.getElementById("email-input").style.borderColor = "white";
+  document.getElementById("email-submit-status").innerHTML = "";
+});
+
+document.getElementById("submit-email-input").addEventListener("click", () => {
+  var email = document.getElementById("email-input").value;
+  if (ValidateEmail(email)) {
+    document.getElementById("email-input").value = "";
+    document.getElementById("email-submit-status").style.color = "#55e061";
+    document.getElementById("email-submit-status").innerHTML =
+      "* Email Submitted";
+  } else {
+    document.getElementById("email-input").style.borderColor = "red";
+    document.getElementById("email-submit-status").style.color = "red";
+    document.getElementById("email-submit-status").innerHTML =
+      "* Invalid Email";
   }
 });
